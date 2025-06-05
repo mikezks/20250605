3a0da3b59c835b652061d60e14be4b390a21f091
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-06-05 13:50:49 +0200

    demo: injection context error

--- projects/flight/src/app/booking/feature-flight/flight-search/flight-search.component.ts
+++ projects/flight/src/app/booking/feature-flight/flight-search/flight-search.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EnvironmentInjector, inject, Injector, OnInit, runInInjectionContext } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Flight, FlightFilter, injectTicketsFacade } from '../../logic-flight';
 import { FlightCardComponent, FlightFilterComponent } from '../../ui-flight';
+import { FlightService } from '../../api-boarding';
 
 
 @Component({
@@ -15,8 +16,9 @@ import { FlightCardComponent, FlightFilterComponent } from '../../ui-flight';
   ],
   templateUrl: './flight-search.component.html',
 })
-export class FlightSearchComponent {
+export class FlightSearchComponent implements OnInit {
   private ticketsFacade = injectTicketsFacade();
+  private injector = inject(EnvironmentInjector);
 
   protected filter = {
     from: 'London',
@@ -28,6 +30,16 @@ export class FlightSearchComponent {
     5: true
   };
   protected flights$ = this.ticketsFacade.flights$;
+
+  constructor() {
+  }
+  
+  ngOnInit(): void {
+    runInInjectionContext(
+      this.injector,
+      () => inject(FlightService)
+    ).findById(10).subscribe(console.log);
+  }
 
   protected search(filter: FlightFilter): void {
     this.filter = filter;
