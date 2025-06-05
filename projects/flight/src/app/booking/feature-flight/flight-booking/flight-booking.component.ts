80b0d620af0947e68f4b44e93591360fcef9e448
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-06-05 14:51:51 +0200

    feat: local navigation config provider

--- projects/flight/src/app/booking/feature-flight/flight-booking/flight-booking.component.ts
+++ projects/flight/src/app/booking/feature-flight/flight-booking/flight-booking.component.ts
@@ -1,17 +1,41 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from '../../../shared/ui-common';
+import { provideNavigationService } from '../../../shared/logic-navigation';
+import { NavigationProviderDirective } from '../../../shared/ui-common/navigation/navigation-provider.directive';
 
 
 @Component({
   selector: 'app-flight-booking',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    NavigationComponent,
+    NavigationProviderDirective
   ],
   template: `
+    <app-navigation class="nav-standalone" />
+    <app-navigation [navConfig]="navState" class="nav-standalone" />
+
     <div>
       <router-outlet></router-outlet>
     </div>
   `,
+  providers: [
+    provideNavigationService([
+      {
+        route: 'flight/search',
+        label: 'Flight Search',
+        icon: 'flight'
+      }
+    ])
+  ]
 })
 export class FlightBookingComponent {
+  navState = [
+      {
+        route: 'flight/departures',
+        label: 'Departures',
+        icon: 'departures'
+      }
+    ]
 }
