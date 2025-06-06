62ae4c183ddf093296ac428828fecfae7be64ef3
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-06-06 10:48:46 +0200

    fix: tidy-up

--- projects/flight/src/app/app.config.ts
+++ projects/flight/src/app/app.config.ts
@@ -1,18 +1,18 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideEffects } from '@ngrx/effects';
 import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
-import { APP_ROUTES } from './app.routes';
-import { provideRouterFeature } from './shared/logic-router-state';
-import { provideApiBaseUrl, provideInitialFlights } from './app.providers';
-import { provideNavigationService } from './shared/logic-navigation';
 import { APP_NAVIGATION } from './app.navigation';
+import { provideApiBaseUrl } from './app.providers';
+import { APP_ROUTES } from './app.routes';
+import { provideNavigationService } from './shared/logic-navigation';
+import { provideRouterFeature } from './shared/logic-router-state';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideExperimentalZonelessChangeDetection(),
+    // provideExperimentalZonelessChangeDetection(),
     provideRouter(APP_ROUTES,
       withComponentInputBinding(),
     ),
--- projects/flight/src/app/booking/feature-flight/flight-search/flight-search.component.ts
+++ projects/flight/src/app/booking/feature-flight/flight-search/flight-search.component.ts
@@ -31,12 +31,10 @@ export class FlightSearchComponent {
     5: true
   };
   protected flights = this.ticketsFacade.flights;
-  protected firstname = 'Sarah';
 
   constructor() {
     effect(() => console.log(this.route()));
     effect(() => this.search());
-    setTimeout(() => this.firstname = 'Peter', 3_000);
   }
 
   protected search(): void {
